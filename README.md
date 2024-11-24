f7227215d1a9bc3e7dcc6858303abe901ce41689
Gabrielito666 <galileo.fandango@gmail.com>
2024-11-24 19:35:04 -0300

    first-version-

---
+++ README.md
@@ -0,0 +1,5 @@
+# LEX
+This is the first version of LEX. An extremely lightweight alternative to React.
+It supports very similar syntax and together with lex-builder compiles to vanilla javascript.
+See Proof.html to find out more.
+Official documentation is in process as well as the web and compiler.
--- index.js
+++ index.js
@@ -35,13 +35,27 @@ const useState = (initValue) =>
 }
 const useRef = initValue => ({ current: initValue });
 
+const lexElements = Array.from(document.querySelectorAll("[lexid]"));
+
+let counter = 0;
+
 const createElement = (tag, props={}, ...children) =>
 {
     if(!props) props = {};
+    props.lexid = counter;
     let element;
+    let selectMode = false;
+    if(lexElements.length > 0)
+    {
+        element = document.querySelector(`[lexid="${props.lexid}"]`);
+        lexElements.shift();
+        selectMode = true;
+    }
+    
     if(typeof tag === 'string')
     {
-        element = document.createElement(tag);
+        if(!selectMode) element = document.createElement(tag);
+
         if(props.ref && props.ref instanceof Object)
         {
             props.ref.current = element;
@@ -60,77 +74,41 @@ const createElement = (tag, props={}, ...children) =>
             else element.setAttribute(key, value);
         }
         children.reduce((acc, ch) => [...acc, ...Array.isArray(ch) ? ch : [ch]],[])
-        .forEach(ch =>
+        .forEach((ch, i) =>
         {
-            if(ch instanceof window.Node)   element.appendChild(ch);
-            else
+            if(selectMode)
             {
-                const textNode = document.createTextNode(String(ch));
-                element.appendChild(textNode);
                 if(ch instanceof State)
                 {
+                    //En el futuro hay que gestionarlo de mejor manera... esto solo funciona si no hay text nodes asyacentes
+                    const textNode = element.childNodes[i];
                     ch.appendOnChange((newValue) =>{ textNode.nodeValue = newValue })
                 }
             }
-            
+            else
+            {
+                if(ch instanceof window.Node)   element.appendChild(ch);
+                else
+                {
+                    const textNode = document.createTextNode(String(ch));
+                    element.appendChild(textNode);
+                    if(ch instanceof State)
+                    {
+                        ch.appendOnChange((newValue) =>{ textNode.nodeValue = newValue })
+                    }
+                }
+            }
         })
     }
     else if(typeof tag === 'function')
     {
-        element = tag({ ...props, children })
+        if(!selectMode) element = tag({ ...props, children });
     }
-    return element
-}
-    
-let counter = 0;
-
-const selectElement = (tag, props={}, ...children) =>
-{
-    if(!props) props = {};
-    props.lexid = counter;
 
     counter++;
-    if(typeof tag === "string")
-    {
-        const el = document.querySelector(`[lexid="${props.lexid}"]`);
-        if(el)
-        {
-            if(props.ref && props.ref instanceof Object)
-            {
-                props.ref.current = el;
-            }
-            for(let [key, value] of Object.entries(props))
-            {
-                if(key.startsWith("on") && typeof value === "function") el.addEventListener(key.slice(2).toLowerCase(), value);
-                else if(value instanceof State)
-                {
-                    value.appendOnChange((newValue) =>
-                    {
-                        props[key] = newValue;
-                        element.setAttribute(key, newValue);
-                    })
-                }
-                else if(typeof value !== "string"){ el[key] = value }
-            }
-        }
-        children.reduce((acc, ch) => [...acc, ...Array.isArray(ch) ? ch : [ch]],[])
-        .forEach((ch, i) =>
-        {
-            if(ch instanceof State)
-            {
-                //En el futuro hay que gestionarlo de mejor manera... esto solo funciona si no hay text nodes asyacentes
-                const textNode = el.childNodes[i];
-                ch.appendOnChange((newValue) =>{ textNode.nodeValue = newValue })
-            }
-        })
-        return el;
-    }
-    else if(typeof tag === "function")
-    {
-        return tag({ ...props, children })
-    }
+    return element
 }
 
-const Lex = { createElement, selectElement, State, useState, useRef };
+const Lex = { createElement, State, useState, useRef };
 
 export default Lex;
